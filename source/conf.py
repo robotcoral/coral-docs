e8abbeb8425b7bbbf99f4be786eeb58ddda179f3
robotcoralci <robotcoralci@users.noreply.github.com>
2021-09-24 21:54:18 +0000

    chore: bump version to 0.1.15

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Robot Coral Team"
 author = "Robot Coral Team"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.14"
+release = "0.1.15"
 
 
 # -- General configuration ---------------------------------------------------
